436e2c6a35bd4dc9f062a20f3eb4f38b555c5c61
Mateusz Petrykowski <matelysz92@gmail.com>
2018-09-18 21:04:34 +0200

    Merge branch 'master' into mateusz
    # Conflicts:
    #	src/main/java/sda/games/chess/Bishop.java
    #	src/main/java/sda/games/chess/King.java
    #	src/main/java/sda/games/chess/Knight.java
    #	src/main/java/sda/games/chess/Pawn.java
    #	src/main/java/sda/games/chess/Queen.java
    #	src/main/java/sda/games/chess/Rook.java

--- src/main/java/sda/games/chess/Knight.java
+++ src/main/java/sda/games/chess/Knight.java
@@ -3,7 +3,18 @@ package sda.games.chess;
 public class Knight extends Figure {
 
     @Override
-    public boolean isMoveValid(Move move){
-        return true;
+    public boolean isMoveValid(Move move) {
+
+        int fromX = move.getFromX();
+        int fromY = move.getFromY();
+        int toX = move.getToX();
+        int toY = move.getToY();
+
+
+        if (toX != fromX - 1 && toX != fromX + 1 && toX != fromX + 2 && toX != fromX - 2 || toY != fromY - 2 && toY != fromY + 2 && toY != fromY - 1 && toY != fromY + 1) {
+            return false;
+        }
+            return true;
+        }
     }
-}
+
--- src/test/java/KnightTest.java
+++ src/test/java/KnightTest.java
@@ -7,18 +7,66 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class KnightTest {
 
     @Test
-    public void ShouldToGoLikeHorse() {
+    public void ShouldMovingNorthEast() {
         Knight knight = new Knight();
-        Move move = new Move(0, 0, 1, 2);
+        Move move = new Move("b5>c3");
+        assertThat(knight.isMoveValid(move)).isTrue();
+    }
+
+    @Test
+    public void ShouldMovingSouthWest() {
+        Knight knight = new Knight();
+        Move move = new Move("b2>a4");
+        assertThat(knight.isMoveValid(move)).isTrue();
+    }
+
+
+    @Test
+    public void ShouldMovingNorthWest() {
+        Knight knight = new Knight();
+        Move move = new Move("d4>c2");
+        assertThat(knight.isMoveValid(move)).isTrue();
+    }
+
+    @Test
+    public void ShouldMovingSouthEast() {
+        Knight knight = new Knight();
+        Move move = new Move("c5>d7");
+        assertThat(knight.isMoveValid(move)).isTrue();
+    }
+
+    @Test
+    public void ShouldNotMovingSouth() {
+        Knight knight = new Knight();
+        Move move = new Move("g3>g4");
         assertThat(knight.isMoveValid(move)).isFalse();
     }
 
     @Test
-    public void ShouldToGoLikeHorse2() {
+    public void ShouldNotMovingFarLikeBishop() {
         Knight knight = new Knight();
-        Move move = new Move(5, 3, 4, 1);
+        Move move = new Move("a2>e6");
         assertThat(knight.isMoveValid(move)).isFalse();
     }
 
+    @Test
+    public void ShouldNotMovingWest() {
+        Knight knight = new Knight();
+        Move move = new Move("h1>a1");
+        assertThat(knight.isMoveValid(move)).isFalse();
+    }
 
+    @Test
+    public void ShouldNotMovingEast() {
+        Knight knight = new Knight();
+        Move move = new Move("b3>f3");
+        assertThat(knight.isMoveValid(move)).isFalse();
+    }
+
+    @Test
+    public void ShouldNotMovingInTheSamePlace() {
+        Knight knight = new Knight();
+        Move move = new Move("a2>a2");
+        assertThat(knight.isMoveValid(move)).isFalse();
+    }
 }
