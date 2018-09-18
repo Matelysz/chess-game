a5d946afc61329fbb5b293f811af40b83be16315
Mariusz Guść <mariuszgusc@gmail.com>
2018-09-18 19:19:23 +0200

    Adding some lines of code to Board and ChessGame and create Display

--- src/main/java/sda/games/chess/ChessGame.java
+++ src/main/java/sda/games/chess/ChessGame.java
@@ -5,8 +5,29 @@ public class ChessGame {
     private Player currentPlayer;
     private Player player1;
     private Player player2;
+    private Board board;
 
-    private void play(){
+    public void play(){
+
+        Player player1 = new Player();
+        Player player2 = new Player();
+        currentPlayer = player1;
+
+        do {
+
+
+
+        } while (board.getGameState().equals(GameState.INPROGRESS));
 
     }
+
+    public Player changeCurrentPlayer(Player currentPlayer) {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player2;
+        }
+        return currentPlayer;
+    }
+
 }
