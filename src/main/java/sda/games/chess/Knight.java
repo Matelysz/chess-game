6d09e720c09d96b7ada62ba6506b16a2e123845a
Mateusz Petrykowski <matelysz92@gmail.com>
2018-09-18 19:20:37 +0200

    no message

--- src/main/java/sda/games/chess/Knight.java
+++ src/main/java/sda/games/chess/Knight.java
@@ -3,7 +3,29 @@ package sda.games.chess;
 public class Knight extends Figure {
 
     @Override
-    public boolean isMoveValid(){
-        return true;
+    public boolean isMoveValid(Move move) {
+
+        int fromX = move.getFromX();
+        int fromY = move.getFromY();
+        int toX = move.getToX();
+        int toY = move.getToY();
+
+        if (toY - fromY == 2 && toX - fromX == 1) {
+            if (toY - fromY == 2 && toX - fromX == -1) {
+                if (toY - fromY == -2 && toX - fromX == 1) {
+                    if (toY - fromY == -2 && toX - fromX == -1) {
+                        return false;
+
+                    }
+                    return false;
+                }
+                return false;
+            }
+            return false;
+        } else {
+            return true;
+
+
+        }
     }
 }
